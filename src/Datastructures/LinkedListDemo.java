5235d7e077eabc83f6d26f770fbfbcdd76b47d15
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-05-19 17:59:34 -0500

    Added LinkedList

---
+++ src/Datastructures/LinkedListDemo.java
@@ -0,0 +1,41 @@
+package Datastructures;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Naren on 5/19/17.
+ */
+public class LinkedListDemo {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> integerList = new LinkedList<>();
+
+        integerList.add(10);
+        integerList.add(80);
+        integerList.add(90);
+        integerList.add(40);
+        integerList.add(50);
+        integerList.add(60);
+
+        integerList.forEach(System.out::println);
+
+        integerList.sort(Integer::compareTo);
+
+        Iterator<Integer> integerIterator = integerList.iterator();
+
+        while (integerIterator.hasNext()) {
+            int x = integerIterator.next();
+            if(x == 50) integerIterator.remove();
+        }
+
+//        integerList.removeIf(x -> x == 50);
+
+        integerList.forEach(System.out::println);
+
+    }
+
+}
