fcffcb4968b65a41eb7d1cc6f90b71fd500893b3
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-05-21 17:22:07 -0500

    Added Random

---
+++ src/Leetcode/RandomDemo.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Naren on 5/21/17.
+ */
+public class RandomDemo {
+
+  public static void main(String[] args) {
+
+    List random = Collections.unmodifiableList(new Random()
+                          .ints(-100, 101)
+                          .limit(2500)
+                          .boxed()
+                          .collect(Collectors.toList()));
+
+    System.out.println(random);
+
+  }
+
+
+}
