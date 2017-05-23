c632abb697a4bc3ec3501bd71d93ca69fed90d67
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-05-22 22:33:34 -0500

    Added Spiral Demo

---
+++ src/Leetcode/SpiralDemo.java
@@ -0,0 +1,57 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Naren on 5/22/17.
+ */
+public class SpiralDemo {
+
+  public List<Integer> spiralOrder(int[][] matrix) {
+
+    List<Integer> res = new ArrayList<>();
+
+    if (matrix.length == 0) {
+      return res;
+    }
+
+    int rowBegin = 0;
+    int rowEnd = matrix.length-1;
+    int colBegin = 0;
+    int colEnd = matrix[0].length - 1;
+
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+      // Traverse Right
+      for (int j = colBegin; j <= colEnd; j ++) {
+        res.add(matrix[rowBegin][j]);
+      }
+      rowBegin++;
+
+      // Traverse Down
+      for (int j = rowBegin; j <= rowEnd; j ++) {
+        res.add(matrix[j][colEnd]);
+      }
+      colEnd--;
+
+      if (rowBegin <= rowEnd) {
+        // Traverse Left
+        for (int j = colEnd; j >= colBegin; j --) {
+          res.add(matrix[rowEnd][j]);
+        }
+      }
+      rowEnd--;
+
+      if (colBegin <= colEnd) {
+        // Traver Up
+        for (int j = rowEnd; j >= rowBegin; j --) {
+          res.add(matrix[j][colBegin]);
+        }
+      }
+      colBegin ++;
+    }
+
+    return res;
+  }
+
+}
