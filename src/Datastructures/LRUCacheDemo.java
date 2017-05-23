ccaf4db0d4ec3c5078d8dcd6f03f0ab2c8843035
Narendra Kumar Manoharan <narenkmanoharan@gmail.com>
2017-05-22 22:32:31 -0500

    Added LRUCache

---
+++ src/Datastructures/LRUCacheDemo.java
@@ -0,0 +1,18 @@
+package Datastructures;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Naren on 5/22/17.
+ */
+public class LRUCacheDemo {
+
+  class LRUCache {
+
+    int capacity;
+
+
+  }
+
+
+}
